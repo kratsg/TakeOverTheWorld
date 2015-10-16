19986db11167927fdbafe45a0363b410c8e82ca7
Giordon Stark <kratsg@gmail.com>
2015-10-16 16:36:57 -0400

    oops

--- totw.py
+++ totw.py
@@ -150,7 +150,7 @@ def get_did(hist):
     raise TypeError("Must pass in a rootpy Hist object")
   m = did_regex.search(hist.get_directory().get_file().name)
   if m is None:
-    raise ValueError("%s is not a valid filename")
+    raise ValueError("%s is not a valid filename" % hist.get_directory().get_file().name)
   return m.groups()[0]
 
 if __name__ == "__main__":
