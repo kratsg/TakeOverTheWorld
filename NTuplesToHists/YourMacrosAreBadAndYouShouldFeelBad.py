a8ec9fea46deb98adb9a78161d893aad9821493d
Giordon Stark <kratsg@gmail.com>
2017-06-06 10:14:13 -0500

    make the outputs in same directory as inputs if requested

--- NTuplesToHists/YourMacrosAreBadAndYouShouldFeelBad.py
+++ NTuplesToHists/YourMacrosAreBadAndYouShouldFeelBad.py
@@ -26,7 +26,7 @@ for f in args.files:
   print "opening {0}".format(f)
   if args.newOutputs:
     in_file = root_open(f, "READ")
-    out_file = root_open("hists_{0:s}".format(f), "RECREATE")
+    out_file = root_open(os.path.join(os.path.dirname(f), "hists_{0:s}".format(os.path.basename(f))), "RECREATE")
     tree = in_file.get(args.treename)
   else:
     out_file = root_open(f, "UPDATE")
